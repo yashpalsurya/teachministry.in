00e35adeced797338b4c01578b0a15c1bcc63675
yashpalsurya <yashpal.surya@cdgi.edu.in>
2024-05-13 15:14:04 +0530

    update

---
+++ shayriData.js
@@ -0,0 +1,30 @@
+const shayriData = [
+    {
+      poet: "Yash",
+      shayri: "तेरी मुस्कान में खो जाऊं, यश, तेरी बातों में खो जाऊं।"
+    },
+    {
+      poet: "Neha",
+      shayri: "जीवन के बगीचे में, नेहा फूल की तरह खिलती है।"
+    },
+    {
+      poet: "Karan",
+      shayri: "मेरे दिल के सितारों में बसा है तेरा नाम।"
+    },
+    {
+      poet: "Omkar",
+      shayri: "ओमकर, तेरे सपनों में बसा है मेरा मन।"
+    },
+    {
+      poet: "Ragni",
+      shayri: "रागनी, तेरी मुस्कान में है मेरी खुशियों की छाँव।"
+    },
+    {
+      poet: "Mahima",
+      shayri: "महिमा, तेरी आँखों में बसा है मेरा सपना।"
+    }
+  ];
+  
+  // Export the shayriData array to make it accessible from other files
+  export default shayriData;
+  
