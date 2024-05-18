841845a823560e737cc5a9748d679044fc366a3d
yashpalsurya <yashpal.surya@cdgi.edu.in>
2024-05-18 12:35:34 +0530

    change

--- shayriData.js
+++ shayriData.js
@@ -1,30 +1,36 @@
 const shayriData = [
     {
       poet: "Yash",
-      shayri: "तेरी मुस्कान में खो जाऊं, यश, तेरी बातों में खो जाऊं।"
+      shayri: "तेरी मुस्कान में खो जाऊं, यश, तेरी बातों में खो जाऊं।",
+      typr: "love"
+
     },
     {
       poet: "Neha",
-      shayri: "जीवन के बगीचे में, नेहा फूल की तरह खिलती है।"
+      shayri: "जीवन के बगीचे में, नेहा फूल की तरह खिलती है।",
+      typr: "love"
     },
     {
       poet: "Karan",
-      shayri: "मेरे दिल के सितारों में बसा है तेरा नाम।"
+      shayri: "मेरे दिल के सितारों में बसा है तेरा नाम।",
+      typr: "sad"
     },
     {
       poet: "Omkar",
-      shayri: "ओमकर, तेरे सपनों में बसा है मेरा मन।"
+      shayri: "ओमकर, तेरे सपनों में बसा है मेरा मन।",
+      typr: "emotional"
     },
     {
       poet: "Ragni",
-      shayri: "रागनी, तेरी मुस्कान में है मेरी खुशियों की छाँव।"
+      shayri: "रागनी, तेरी मुस्कान में है मेरी खुशियों की छाँव।",
+      typr: "feeling"
     },
     {
       poet: "Mahima",
-      shayri: "महिमा, तेरी आँखों में बसा है मेरा सपना।"
+      shayri: "महिमा, तेरी आँखों में बसा है मेरा सपना।",
+      typr: "care"
     }
   ];
   
-  // Export the shayriData array to make it accessible from other files
-  export default shayriData;
+
   
